dc3194496da941c1e4a90c23fb0461daaf129ce9
bunicholas <74833401+bunicholas@users.noreply.github.com>
2021-09-17 14:42:01 -0400

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,20 +1,19 @@
 const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://pokeapi.co/docs/v2", true)
-xhr.send()
+xhr.open("GET", "https://pokeapi.co/api/v2/ability/1", true)
+xhr.send();
 xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
         let data = JSON.parse(xhr.responseText)
-        showData(data.results)
-    } else if (xhr.readyState === 4 && xhr.status !== 200){
+        showData(data.names)
+    } else if (xhr.readyState === 4 && xhr.status === 200){
         console.log(xhr.responseText)
     }
 }
 
 function showData(data) {
     console.log(data)
-    for(let i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         const character = document.createElement('h1')
-        character.textContent = data[i].name
+        character.textContent = data[i].language.name
         document.body.appendChild(character)
-    }
 }
