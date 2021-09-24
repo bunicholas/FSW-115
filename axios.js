665d71151464b54419122632c317bfd0df1ec153
bunicholas <nicholascarroll123@gmail.com>
2021-09-23 22:37:33 -0400

    M

--- axios.js
+++ axios.js
@@ -1,18 +1,46 @@
-axios.get('http://api.bryanuniversity.edu/<nickcarroll>/list')
-.then(response => {
+// axios.get('http://api.bryanuniversity.edu/<nickcarroll>/list')
+// .then(response => {
+//     for(let i = 0; i < response.data.length; i++){
+//         const h1 = document.createElement('h1')
+//         h1.style.color = "red";
+//         h1.textContent = response.data[i].name
+//         document.body.appendChild(h1)
+//     };
+// })
+function page1(){
+    console.log('ran page 1')
+axios.get('https://swapi.dev/api/people/1')
+.then(response =>  {
+    console.log(response.data)
     for(let i = 0; i < response.data.length; i++){
-        const h1 =document.createElement('h1')
+        const h1 = document.createElement('h1')
+        h1.style.color = "red";
         h1.textContent = response.data[i].name
         document.body.appendChild(h1)
     };
 })
+.catch()
+}
 var button = document.getElementById("button");
+console.log(button)
+button.addEventListener("click", page1);
+
+var button2 = document.getElementById("button2");
 
 function handleButtonClick() {
     console.log("The button was clicked");
+    axios.get('http://api.bryanuniversity.edu/<nickcarroll>/list')
+.then(response => {
+    for(let i = 0; i < response.data.length; i++){
+        const h1 = document.createElement('h1')
+        h1.style.color = "red";
+        h1.textContent = response.data[i].name
+        document.body.appendChild(h1)
+    };
+})
 }
 
-button.addEventListener("click", handleButtonClick);
+button2.addEventListener("click", handleButtonClick);
 
 document.getElementById("header").addEventListener("dblclick", function() {
     document.body.style.backgroundColor = "green"
