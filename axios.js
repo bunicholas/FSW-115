0bfcddcba26741b662811bbdac019aa53ec53358
bunicholas <nicholascarroll123@gmail.com>
2021-09-23 22:57:24 -0400

    M

--- axios.js
+++ axios.js
@@ -25,26 +25,3 @@ var button = document.getElementById("button");
 console.log(button)
 button.addEventListener("click", page1);
 
-var button2 = document.getElementById("button2");
-
-function handleButtonClick() {
-    console.log("The button was clicked");
-    axios.get('http://api.bryanuniversity.edu/<nickcarroll>/list')
-.then(response => {
-    for(let i = 0; i < response.data.length; i++){
-        const h1 = document.createElement('h1')
-        h1.style.color = "red";
-        h1.textContent = response.data[i].name
-        document.body.appendChild(h1)
-    };
-})
-}
-
-button2.addEventListener("click", handleButtonClick);
-
-document.getElementById("header").addEventListener("dblclick", function() {
-    document.body.style.backgroundColor = "green"
-});
-document.querySelector("h1").style.color = "red";
-document.querySelector("script").style.color = "red";
-//.catch(error => console.log(error))
